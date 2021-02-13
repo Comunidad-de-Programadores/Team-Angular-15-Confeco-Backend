36cc0f8ee3a5bc64d6488e15de38baaa2b750ae7
Ivan Zaldivar <ivanzaldivar16@gmail.com>
2021-02-12 21:20:54 -0600

    Some comments were removed.

--- src/app/config/environments.ts
+++ src/app/config/environments.ts
@@ -1,7 +1,6 @@
 // Imports modules.
 import { config } from "dotenv";
 
-// Verify env.
 if (process.env.NODE_ENV !== "production") config();
 
 export const environments = {
--- src/app/database/mongo/repositories/UserRepositoryMongo.ts
+++ src/app/database/mongo/repositories/UserRepositoryMongo.ts
@@ -6,27 +6,15 @@ import { IDatabaseUserRepository } from "../../../interfaces/repositories.interf
 import { User } from "../../../models/User";
 
 export class UserRepositoryMongo implements IDatabaseUserRepository {
-    /**
-     * 🧔 Save a new user.
-     * @param user 
-     */
     async create(user: User): Promise<void> {
         await new models.User(user).save();
     }
 
-    /**
-     * 🆔 Get user by id.
-     * @param id 
-     */
     async get(id: string): Promise<User | null> {
         const data: any = await models.User.findById(id, { __v: 0 });
         return data ? new User(data) : null;
     }
 
-    /**
-     * 📧 Get user by email.
-     * @param email 
-     */
     async getByEmail(email: string): Promise<User | null> {
         const data: any = await models.User.findOne({ email });
         return data ? new User(data) : null;
