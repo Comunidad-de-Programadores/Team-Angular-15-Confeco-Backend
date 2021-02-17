1cc92495b79ce5eb1d600656b13ccaafb9a22dcc
Ivan Zaldivar <ivanzaldivar16@gmail.com>
2021-02-17 16:41:38 -0600

    Updated routes

--- src/app/routes/index.routes.ts
+++ src/app/routes/index.routes.ts
@@ -6,21 +6,6 @@ import { routes } from "./routes";
 
 export class IndexRoutes {
     constructor(public main: Express) {
-        this.routes();
-        this.notFound();
-    }
-    
-    private routes(): void {
         routes.forEach(route => this.main.use("/api", route));
-    }
-    
-    private notFound(): void {        
-        this.main.get("**", (req, res) => {
-            res.status(404).json({
-                code: 404,
-                name: "NotFound",
-                message: "El recurso solicitado no fue encontrado."
-            });
-        });
     }
 }
