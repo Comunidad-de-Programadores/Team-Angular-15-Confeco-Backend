49768bc8981395c76ecf15faad65797d336201fb
Ivan Zaldivar <ivanzaldivar16@gmail.com>
2021-02-17 01:55:46 -0600

    The driver was modified and AuthPostman was removed

--- src/app/components/auth/auth.controller.ts
+++ src/app/components/auth/auth.controller.ts
@@ -4,10 +4,6 @@ import { Request, Response } from "express";
 // Imports interfaces.
 import { IAuthRes } from "../../interfaces/auth.interfaces";
 
-// Imports postman
-import { AuthPostmanComponent } from "./auth.postman";
-const authPostman = new AuthPostmanComponent();
-
 // Imports facades.
 import { AuthFacade } from "../../facade/auth/auth.facade";
 const auth = new AuthFacade();
@@ -16,7 +12,7 @@ export class AuthControllerComponents {
     async register(req: Request, res: Response): Promise<void> {
         try {
             const { nickname } = req.body;
-            await authPostman.register(req);
+            await auth.register(req.body);
             res.status(200).json({ message: `¡Felicidades ${ nickname }! Te has registrado correctamente. Te hemos enviado un email de confirmacion para verificar que eres tu.` });
         } catch (error) {
             const { name, message, statusCode } = error;
@@ -26,7 +22,7 @@ export class AuthControllerComponents {
 
     async login(req: Request, res: Response): Promise<void> {
         try {
-            const data: IAuthRes = await authPostman.login(req);
+            const data: IAuthRes = await auth.login(req.body);
             res.status(200).json(data);
         } catch (error) {
             const { name, message, statusCode } = error;
@@ -36,7 +32,7 @@ export class AuthControllerComponents {
 
     async verificationEmail(req: Request, res: Response): Promise<void> {
         try {
-            const { user }: IAuthRes = await authPostman.verifyEmail(req);
+            const { user }: IAuthRes = await auth.verifyEmail(req.params.token);
             res.render("success/verifiedEmail", {
                 nickname: user.nickname
             });
@@ -47,7 +43,7 @@ export class AuthControllerComponents {
 
     async forgotPassword(req: Request, res: Response): Promise<void> {
         try {
-            await authPostman.forgotPassword(req);
+            await auth.forgotPassword(req.body.email);
             res.status(200).json({ message: "Se ha enviado un email a tu cuenta de correo electronico para que puedas modificar tu contraseña." });
         } catch (error) {
             const { name, message, statusCode } = error;
@@ -71,7 +67,7 @@ export class AuthControllerComponents {
 
     async resetPassword(req: Request, res: Response): Promise<void> {
         try {
-            await authPostman.resetPassword(req);
+            await auth.resetPassword(req.body);
             res.render("success/resetPassword");
         } catch (error) {
             res.status(400).render("errors/resetPassword", {
@@ -82,7 +78,7 @@ export class AuthControllerComponents {
 
     async google(req: Request, res: Response): Promise<void> {
         try {
-            const data: IAuthRes = await authPostman.google(req);
+            const data: IAuthRes = await auth.google(req.body.token);
             res.status(200).json(data);
         } catch (error) {
             const { name, message, statusCode } = error;
