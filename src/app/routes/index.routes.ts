452309cf1ec432a377095128cf2bbf3f76c71697
Ivan Zaldivar <ivanzaldivar16@gmail.com>
2021-02-19 02:28:35 -0600

    404 route added

--- src/app/routes/index.routes.ts
+++ src/app/routes/index.routes.ts
@@ -6,6 +6,24 @@ import { routes } from "./routes";
 
 export class IndexRoutes {
     constructor(public main: Express) {
-        routes.forEach(route => main.use(route.path, route.component));
+        this.executeRoutes();
+        this.notFound();
+    }
+
+    private executeRoutes(): void {
+        routes.forEach(route => this.main.use(route.path, route.component));
+    }
+
+    private notFound(): void {
+        this.main.get("*", (req, res, next) => {
+            if (req.headers.accept?.includes("html")) {
+                return res.status(404).json({
+                    code: 404,
+                    name: "NotFound",
+                    message: "El recurso solicitado no existe."
+                });
+            }
+            return next();
+        });
     }
 }
