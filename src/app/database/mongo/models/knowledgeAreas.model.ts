9868be912e521383a4d735900f45d41575a7b5de
Ivan Zaldivar <ivanzaldivar16@gmail.com>
2021-02-21 10:50:06 -0600

    New models were added.

--- src/app/database/mongo/models/knowledgeAreas.model.ts
+++ src/app/database/mongo/models/knowledgeAreas.model.ts
@@ -1,6 +1,16 @@
 // Imports modules.
 import { Schema, model } from "mongoose";
 
-const knowledgeAreaSchema = new Schema({});
+const knowledgeAreaSchema = new Schema({
+    _id: { type: String, required: true },
+    title: { type: String, required: true },
+    subtitle: { type: String },
+    description: { type: String },
+    workshops: [{ type: String, ref: "workshops", autopopulate: true }],
+    banner: { type: String },
+    profile_picture: { type: String },
+    created_at: { type: Date, default: new Date() },
+    updated_at: { type: Date, default: new Date() }
+});
 
 export const knowledgeAreaModel = model("knowledge_areas", knowledgeAreaSchema);
---
+++ src/app/models/KnowledgeArea.ts
@@ -0,0 +1,26 @@
+// Imports models.
+import { Workshop } from "./Workshop";
+
+export class KnowledgeArea {
+    _id: string;
+    title: string;
+    subtitle?: string;
+    description?: string;
+    workshops?: Workshop[];
+    banner?: string;
+    profile_picture?: string;
+    created_at?: Date;
+    updated_at?: Date;
+
+    constructor(data: KnowledgeArea) {
+        this._id = data._id;
+        this.title = data.title;
+        this.subtitle = data.subtitle;
+        this.description = data.description;
+        this.workshops = data.workshops || [];
+        this.banner = data.banner;
+        this.profile_picture = data.profile_picture;
+        this.created_at = data.created_at || new Date();
+        this.updated_at = data.updated_at || new Date();
+    }
+};
