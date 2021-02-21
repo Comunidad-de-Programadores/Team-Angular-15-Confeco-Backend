944fffad4645dbff213adc17f5c0d851353fa153
Ivan Zaldivar <ivanzaldivar16@gmail.com>
2021-02-21 14:41:30 -0600

    The properties of the workshops model was added

--- src/app/database/mongo/models/workshop.model.ts
+++ src/app/database/mongo/models/workshop.model.ts
@@ -1,6 +1,13 @@
 // Imports modules.
 import { Schema, model } from "mongoose";
 
-const workshopSchema = new Schema({});
+const workshopSchema = new Schema({
+    _id: { type: String, required: true },
+    title: { type: String, required: true },
+    subtitle: { type: String, required: true },
+    workshop_time: { type: Date, required: true },
+    workshop_duration: { type: Date, required: true },
+    instructor: { type: Object, required: true }
+});
 
 export const WorkshopModel = model("workshops", workshopSchema);
