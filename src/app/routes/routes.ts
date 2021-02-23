f3577e074a5b7945dd088340dd479ee3ce612328
Ivan Zaldivar <ivanzaldivar16@gmail.com>
2021-02-22 23:18:55 -0600

    The instructors component has been removed.

--- src/app/routes/routes.ts
+++ src/app/routes/routes.ts
@@ -5,11 +5,9 @@ import { IRouter } from "./interfaces/routes.interfaces";
 import { authRoutes } from "../components/auth/auth.routes";
 import { workshopsRoutes } from "../components/workshops/workshops.routes";
 import { knowledgeRoutes } from "../components/knowledge_areas/KnowledgeArea.routes";
-import { instructorRoutes } from "../components/instructors/instructor.routes";
 
 export const routes: IRouter[] = [
     authRoutes,
     workshopsRoutes,
-    knowledgeRoutes,
-    instructorRoutes
+    knowledgeRoutes
 ];
