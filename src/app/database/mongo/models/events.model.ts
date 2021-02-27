a666a2e17084013e305fce84c667dfb96d7e383a
Ivan Zaldivar <ivanzaldivar16@gmail.com>
2021-02-27 00:20:41 -0600

    The properties of the events model were established in the database.

--- src/app/database/mongo/models/events.model.ts
+++ src/app/database/mongo/models/events.model.ts
@@ -1,6 +1,27 @@
 // Imports modules.
 import { Schema, model } from "mongoose";
 
-const eventSchema: Schema = new Schema({});
+const eventSchema: Schema = new Schema({
+    _id: { type: String, required: true },
+    title: { type: String, required: true },
+    subtitle: { type: String },
+    description: { type: String },
+    logo: { type: String },
+    banner: { type: String },
+    members: [{
+        type: String,
+        ref: "users",
+        autopopulate: {
+            select: {
+                password: 0,
+                passwordResetToken: 0
+            }
+        }
+    }],
+    expedition_date: { type: Date, required: true },
+    expiration_date: { type: Date, required: true },
+    created_at: { type: String, default: () => new Date },
+    updated_at: { type: String, default: () => new Date }
+});
 
 export const eventModel = model("events", eventSchema);
