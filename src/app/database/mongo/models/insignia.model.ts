8ca0022355bc91bed2f4ad56d25ba1ca2edba519
Ivan Zaldivar <ivanzaldivar16@gmail.com>
2021-02-27 00:25:06 -0600

    The properties of the insignia model were established in the database.

--- src/app/database/mongo/models/insignia.model.ts
+++ src/app/database/mongo/models/insignia.model.ts
@@ -1,6 +1,24 @@
 // Imports modules.
 import { Schema, model } from "mongoose";
 
-const insigniaSchema: Schema = new Schema({});
+const insigniaSchema: Schema = new Schema({
+    _id: { type: String, required: true },
+    title: { title: String, required: true },
+    subtitle: { title: String },
+    description: { title: String, required: true },
+    logo: { type: String },
+    users: [{
+        type: String,
+        ref: "users",
+        autopopulate: {
+            select: {
+                password: 0,
+                passwordResetToken: 0
+            }
+        }
+    }],
+    created_at: { type: Date, default: () => new Date },
+    updated_at: { type: Date, default: () => new Date }
+});
 
 export const insigniaModel = model("events", insigniaSchema);
