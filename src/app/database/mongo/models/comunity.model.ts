ed6c2ef4362f5a47a0ff09712f56b81f661a902d
Ivan Zaldivar <ivanzaldivar16@gmail.com>
2021-02-27 00:07:56 -0600

    The properties of the community model were established

--- src/app/database/mongo/models/comunity.model.ts
+++ src/app/database/mongo/models/comunity.model.ts
@@ -1,6 +1,25 @@
 // Imports modules.
 import { Schema, model } from "mongoose";
 
-const comunitySchema: Schema = new Schema({});
+const comunitySchema: Schema = new Schema({
+    _id: { type: String, required: true },
+    title: { type: String, required: true },
+    subtitle: { type: String },
+    description: { type: String },
+    members: [{
+        type: String,
+        ref: "users",
+        autopopulate: {
+            select: {
+                password: 0,
+                passwordResetToken: 0
+            }
+        }
+    }],
+    logo: { type: String },
+    banner: { type: String },
+    created_at: { type: Date, default: () => new Date },
+    updated_at: { type: Date, default: () => new Date }
+});
 
 export const comunityModel = model("comunities", comunitySchema);
