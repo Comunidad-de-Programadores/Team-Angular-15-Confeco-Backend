518a27b76d516f4186fa18367183647024d6a17a
Ivan Zaldivar <ivanzaldivar16@gmail.com>
2021-02-27 12:18:43 -0600

    The workshop.postman.ts file was modified

--- src/app/components/workshops/workhops.postman.ts
+++ src/app/components/workshops/workhops.postman.ts
@@ -9,17 +9,34 @@ import { IWorkshopRepository } from "../../database/interfaces/repositories.inte
 // Imports repositories.
 import { WorkshopRepositoryMongo } from "../../database/mongo/repositories/WorkshopRepositoryMongo";
 
+// Imports jsonwebtoken.
+import { JsonWebToken } from "../../helpers/jsonwebtokens/JsonWebToken";
+import { JwtAccessToken } from "../../helpers/jsonwebtokens/strategies/AccessToken";
+import { User } from "../../models/User";
+
 export class WorkshopPostman {
     private repository: IWorkshopRepository;
+    private jwt: JsonWebToken;
 
     constructor() {
         this.repository = new WorkshopRepositoryMongo;
+        this.jwt = new JwtAccessToken;
     }
 
     async create(req: Request) {
         // Save in the database.
         const _id = uuid();
-        await this.repository.create({ _id, ...req.body });
+
+        // We get the token.
+        const authorization = req.headers.authorization as string;
+        const token: string = authorization.replace("Bearer ", "");
+
+        // We obtain the user's information.
+        const payload: User = this.jwt.verify(token, new JwtAccessToken);
+        const instructor: string = payload._id;
+
+        // Save user in the database.
+        await this.repository.create({ _id, instructor, ...req.body });
 
         // Get data
         return await this.repository.get(_id);
@@ -27,16 +44,15 @@ export class WorkshopPostman {
 
     async list(req: Request) {
         // Get one.
-        const { id } = req.params;
-        if (id) {
-            const workshop = await this.repository.get(id)
+        if (req.params.id) {
+            const workshop = await this.repository.get(req.params.id);
             
             if (!workshop) throw createHttpError(404, "El recurso solicitado no existe.", {
                 name: "NotFount"
             });
 
             return workshop;
-        };
+        }
 
         // Get list
         const { limit, skip } = req.query;
