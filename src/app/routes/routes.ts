9285c4e949aa17ec0831206039951f1e6adfeada
Ivan Zaldivar <ivanzaldivar16@gmail.com>
2021-03-15 01:51:41 -0600

    Several components were removed.

--- src/app/routes/routes.ts
+++ src/app/routes/routes.ts
@@ -3,17 +3,9 @@ import { IRouter } from "./interfaces/routes.interfaces";
 
 // Imports components routes.
 import { authRoutes } from "../components/auth/auth.routes";
-import { workshopsRoutes } from "../components/workshops/workshops.routes";
-import { knowledgeRoutes } from "../components/knowledge_areas/KnowledgeArea.routes";
 import { userRoutes } from "../components/users/user.routes";
-import { comunitiesRoutes } from "../components/comunities/comunities.routes";
-import { eventsRoutes } from "../components/events/events.routes";
 
 export const routes: IRouter[] = [
     authRoutes,
-    userRoutes,
-    workshopsRoutes,
-    knowledgeRoutes,
-    comunitiesRoutes,
-    eventsRoutes
+    userRoutes
 ];
