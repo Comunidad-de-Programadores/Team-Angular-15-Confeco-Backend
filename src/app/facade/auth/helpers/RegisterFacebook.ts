6f348823214c92616ab2d9cb3ddc3ac0c99f2718
Ivan Zaldivar <ivanzaldivar16@gmail.com>
2021-03-17 14:12:55 -0600

    Added earning badges upon registration

--- src/app/facade/auth/helpers/RegisterFacebook.ts
+++ src/app/facade/auth/helpers/RegisterFacebook.ts
@@ -8,6 +8,7 @@ import { UserDatabase } from "../../../repositories/interfaces/entities.interfac
 
 // Imports models.
 import { User } from "../../../models/User";
+import { BadgeUser } from "../../../models/badges/BadgeUser";
 
 // Imports facades.
 import { JwtFacade } from "../../Jwt/JwtFacade";
@@ -16,12 +17,16 @@ import { JwtFacade } from "../../Jwt/JwtFacade";
 import { DatabaseRepository } from "../../../repositories/DatabaseRepository";
 import { CreateUser } from "../../../repositories/user/write.user";
 import { GetUser } from "../../../repositories/user/read.user";
+import { WinBadge } from "../../../repositories/badges/write.badge";
+import { environments } from "../../../config/environments";
 
 export class RegisterFacebook implements IAuth<IAuthRes> {
+    private databaseBadge: DatabaseRepository<string, BadgeUser>;
     private database: DatabaseRepository<string, UserDatabase>;
     private jwt: JwtFacade;
     
     constructor(private data: { last_name: string, email: string | undefined }) {
+        this.databaseBadge = new DatabaseRepository;
         this.database = new DatabaseRepository;
         this.jwt = new JwtFacade();
     }
@@ -46,6 +51,13 @@ export class RegisterFacebook implements IAuth<IAuthRes> {
         if (!user) throw createHttpError(401, "Ha sucedido un error durante la operacion.", {
             name: "AuthenticationError"
         });
+
+        // Win badge
+        await this.databaseBadge.create({
+            _id: uuid(),
+            userId: user._id,
+            badgeId: environments.BADGE_GENESIS_ID as string
+        }, new WinBadge);
 
         // Generate tokens.
         const newUser = Object.assign({}, new User(user));
--- src/app/facade/auth/helpers/RegisterGoogle.ts
+++ src/app/facade/auth/helpers/RegisterGoogle.ts
@@ -4,12 +4,16 @@ import createHttpError from "http-errors";
 import { v4 as uuid } from "uuid";
 import uniqid from "uniqid";
 
+// Imports environments.
+import { environments } from "../../../config/environments";
+
 // Imports interfaces.
 import { IAuth, IAuthRes } from "../interfaces/auth.interfaces";
 import { UserDatabase } from "../../../repositories/interfaces/entities.interfaces";
 
 // Imports models.
 import { User } from "../../../models/User";
+import { BadgeUser } from "../../../models/badges/BadgeUser";
 
 // Imports jsonwebtokens.
 import { JwtFacade } from "../../Jwt/JwtFacade";
@@ -18,20 +22,24 @@ import { JwtFacade } from "../../Jwt/JwtFacade";
 import { DatabaseRepository } from "../../../repositories/DatabaseRepository";
 import { GetUserByEmail } from "../../../repositories/user/read.user";
 import { CreateUser } from "../../../repositories/user/write.user";
+import { WinBadge } from "../../../repositories/badges/write.badge";
 
 export class RegisterGoogle implements IAuth<IAuthRes> {
     private database: DatabaseRepository<string, UserDatabase>;
+    private databaseBadge: DatabaseRepository<string, BadgeUser>;
     private jwt: JwtFacade;
 
     constructor(private data: TokenPayload) {
+        this.databaseBadge = new DatabaseRepository;
         this.database = new DatabaseRepository;
         this.jwt = new JwtFacade();
     }
 
     async auth(): Promise<IAuthRes> {
-        // Save user.
         const nickname: string = `${ this.data.given_name }${ Math.round(Math.random() * (1000 - 100)) }`;
         const email: string = this.data.email || `${ uniqid() }@gmail.com`;
+        
+        // Save user.
         await this.database.create({
             email,
             nickname,
@@ -45,6 +53,13 @@ export class RegisterGoogle implements IAuth<IAuthRes> {
         if (!user) throw createHttpError(400, "Sucedio un error durante la operacion", {
             name: "ErrorRegister"
         });
+
+        // Win badge
+        await this.databaseBadge.create({
+            _id: uuid(),
+            userId: user._id,
+            badgeId: environments.BADGE_GENESIS_ID as string
+        }, new WinBadge);
 
         // Generate tokens.
         const newUser = Object.assign({}, new User(user));
